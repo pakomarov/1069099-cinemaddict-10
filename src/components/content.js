3664314a3fdf7d09a6d503c09b9376a9a0bf6976
Pavel Komarov <komarov.pa.it@gmail.com>
2020-02-01 00:30:21 +0300

    Переписывает на классы раздел с контентом

--- src/components/content.js
+++ src/components/content.js
@@ -1,10 +1,28 @@
-const createContentMarkup = () => {
+import {createElement} from '../utils.js';
+
+const createContentTemplate = () => {
   return `<section class="films">
-    <section class="films-list">
-      <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-      <div class="films-list__container"></div>
-    </section>
   </section>`;
 };
 
-export {createContentMarkup};
+export default class Content {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createContentTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
