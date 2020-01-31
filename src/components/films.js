350fd5f4413bf5962de7506863db0b93a3cc48e6
Pavel Komarov <komarov.pa.it@gmail.com>
2020-02-01 00:36:14 +0300

    Переписывает на классы контейнер с фильмами каталога

---
+++ src/components/films.js
@@ -0,0 +1,27 @@
+import {createElement} from '../utils.js';
+
+const createFilmsTemplate = () => {
+  return `<div class="films-list__container"></div>`;
+};
+
+export default class Films {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createFilmsTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
